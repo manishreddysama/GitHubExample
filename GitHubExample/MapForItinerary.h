6e2335ef719a2c71b2359df8779ddc6eeb7874bb
manishreddysama <manishsama08@gmail.com>
2015-08-21 08:30:56 -0500

    Map Class is added

---
+++ GitHubExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitHubExample
+//
+//  Created by manish sama on 8/21/15.
+//  Copyright (c) 2015 Manish Sama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitHubExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitHubExample
+//
+//  Created by manish sama on 8/21/15.
+//  Copyright (c) 2015 Manish Sama. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
