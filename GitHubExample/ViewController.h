6cc8228f5c2c86ab9b219d14c4945ab3dd753b5f
manishreddysama <manishsama08@gmail.com>
2015-08-21 00:42:21 -0500

    Add log message to console Add button that log's the console when tapped

--- GitHubExample/ViewController.h
+++ GitHubExample/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)clickHere:(id)sender;
+
 
 @end
 
--- GitHubExample/ViewController.m
+++ GitHubExample/ViewController.m
@@ -24,4 +24,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clickHere:(id)sender {
+    
+    NSLog(@"This is Git Hub Tutorial");
+    
+}
 @end
